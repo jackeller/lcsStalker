c7e6c3f1d4664b3fc0d36802c8d2186a6a3852f7
Jack Keller <j3kk3l@gmail.com>
2015-02-17 23:09:49 -0500

    halfway through porting over code from EH

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -9,14 +9,14 @@ describe('controllers', function(){
     scope = $rootScope.$new();
   }));
 
-  it('should define more than 5 awesome things', inject(function($controller) {
-    expect(scope.awesomeThings).toBeUndefined();
+  // it('should define more than 5 awesome things', inject(function($controller) {
+  //   expect(scope.awesomeThings).toBeUndefined();
 
-    $controller('MainCtrl', {
-      $scope: scope
-    });
+  //   $controller('MainCtrl', {
+  //     $scope: scope
+  //   });
 
-    expect(angular.isArray(scope.awesomeThings)).toBeTruthy();
-    expect(scope.awesomeThings.length > 5).toBeTruthy();
-  }));
+  //   expect(angular.isArray(scope.awesomeThings)).toBeTruthy();
+  //   expect(scope.awesomeThings.length > 5).toBeTruthy();
+  // }));
 });
