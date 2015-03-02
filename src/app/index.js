aec62b7aeecebb8b0674292306fbb01c8ed80444
Jack Keller <j3kk3l@gmail.com>
2015-03-01 21:27:24 -0500

    finally got ui-router working

--- src/app/index.js
+++ src/app/index.js
@@ -1,42 +1,23 @@
 'use strict';
 
-angular.module('lcsStalker', [
-        'ui.router',
-        'ngAnimate',
-        'ngCookies',
-        'ngTouch',
-        'ngSanitize',
-        'ngResource',
-        // 'ngRoute',
-        'teams',
-        'teams.players'
-    ])
-
-    // .config(function($stateProvider) {
-    //   $stateProvider
-    //     .state('default', {
-    //         url: '/',
-    //         // templateUrl: 'app/teams/teams.tmpl.html',
-    //         templateUrl: 'app/main/main.html',
-    //         controller: 'MainCtrl'
-
-    //         // .when('/', {
-    //         //     templateUrl: 'app/main/main.html',
-    //         //     controller: 'MainCtrl'
-    //         // })
-    //         // .otherwise({
-    //         //     redirectTo: '/'
-    //         // });
-    //     })
-
-        .config(function ($stateProvider) {
-          $stateProvider
-              .state('default', {
-                  url: '/', // Make to navigate to index.html#/
-                  // templateUrl: 'app/teams/teams.tmpl.html',
-                  templateUrl: 'app/main/main.html',
-                  controller: 'MainCtrl'
-              })
-          ;
+// angular.module('lcsStalker', [
+//         'ui.router',
+//         'ngAnimate',
+//         'ngCookies',
+//         'ngTouch',
+//         'ngSanitize',
+//         'ngResource',
+//         'teams',
+//         'teams.players'
+//     ])
+//         .config(function ($stateProvider) {
+//           $stateProvider
+//               .state('default', {
+//                   url: '/', // Make to navigate to index.html#/
+//                   // templateUrl: 'app/teams/teams.tmpl.html',
+//                   templateUrl: 'app/main/main.html',
+//                   controller: 'MainCtrl'
+//               })
+//           ;
       
-    });
+//     });
