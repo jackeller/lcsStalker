f331ed612f2a9ddd566d631087e10dcf39b27eae
Jack Keller <j3kk3l@gmail.com>
2015-03-14 18:57:27 -0400

    create and edit templates done

--- src/app/teams/players/create/player-create.js
+++ src/app/teams/players/create/player-create.js
@@ -3,4 +3,19 @@
 *
 * Description
 */
-angular.module('teams.players.create', []);
+angular.module('teams.players.create', [
+
+    ])
+        .config(['$stateProvider',function($stateProvider) {
+            $stateProvider
+                .state('default.teams.players.create', {
+                    url: '/players/create',
+                    templateUrl: 'app/teams/players/create/player-create.tmpl.html',
+                    controller: 'CreateBookmarkCtrl as createBookmarkCtrl'
+                });
+        }])
+
+        .controller('CreateBookmarkCtrl', ['', function(){
+            
+        }])
+;
--- src/app/teams/players/edit/player-edit.js
+++ src/app/teams/players/edit/player-edit.js
@@ -3,4 +3,19 @@
 *
 * Description
 */
-angular.module('teams.players.edit', []);
+angular.module('teams.players.edit', [
+
+    ])
+        .config(['$stateProvider',function($stateProvider) {
+            $stateProvider
+                .state('default.teams.players.edit', {
+                    url: '/players/edit',
+                    templateUrl: 'app/teams/players/edit/player-edit.tmpl.html',
+                    controller: 'EditBookmarkCtrl as editBookmarkCtrl'
+                });
+        }])
+
+        .controller('EditBookmarkCtrl', ['', function(){
+            
+        }])
+;
