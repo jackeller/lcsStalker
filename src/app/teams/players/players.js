8f011b48986f189d978f7fc6ac2d727ceca9bdf7
Jack Keller <j3kk3l@gmail.com>
2015-03-22 20:50:15 -0400

    hellloooooo trick2g embedded

--- src/app/teams/players/players.js
+++ src/app/teams/players/players.js
@@ -31,8 +31,8 @@ angular.module('teams.players', [
 
         // <iframe src="http://www.twitch.tv/imaqtpie/embed" frameborder="0" scrolling="no" height="378" width="620"></iframe><a href="http://www.twitch.tv/imaqtpie?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;">Watch live video from imaqtpie on www.twitch.tv</a>
 
-        playersListCtrl.trustURL = function (videoId) {
-          return 'http://www.twitch.tv/' + videoId + "embed";
+        playersListCtrl.trustURL = function (twitchID) {
+          return $sce.trustAsResourceUrl('http://www.twitch.tv/' + twitchID + "/embed");
         };
 
         playersListCtrl.getTwitchOnlineStatus = function( player ) {
