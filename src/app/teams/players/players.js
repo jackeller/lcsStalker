4cd8063747ea607c42d93cf24158bbcabf615528
Jack Keller <j3kk3l@gmail.com>
2015-03-24 08:09:36 -0400

    iframe directive works

--- src/app/teams/players/players.js
+++ src/app/teams/players/players.js
@@ -81,8 +81,8 @@ angular.module('teams.players', [
             // controller: function($scope, $element, $attrs, $transclude) {},
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-            // template: '',
-            templateUrl: '/src/components/iframe/iframe.tmpl.html',
+            template: '<iframe ng-if="player.twitchEmbed" ng-src="{{ playersListCtrl.trustURL( player.twitchEmbed ) }} "></iframe>',
+            // templateUrl: '/src/components/iframe/iframe.tmpl.html',
             // replace: true,
             // transclude: true,
             // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
