57f33a9ac404805527449fd5cdb7dd332c50db6b
Jack Keller <j3kk3l@gmail.com>
2015-03-23 22:34:41 -0400

    working with directives

--- src/app/teams/players/players.js
+++ src/app/teams/players/players.js
@@ -81,13 +81,17 @@ angular.module('teams.players', [
             // controller: function($scope, $element, $attrs, $transclude) {},
             // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
             // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-            template: '<div>foo {{player.name}}</div>',
-            // templateUrl: '',
+            // template: '',
+            templateUrl: '/src/components/iframe/iframe.tmpl.html',
             // replace: true,
             // transclude: true,
             // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
             link: function($scope, iElm, iAttrs, controller) {
                 console.warn("twitch iframe directive");
+
+                if ( $scope.player.twitch == "riotgames" ) {
+                    $scope.player.foo = "raaaaa";
+                }
 
             }
         };
